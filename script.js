1f429d5cae3e2afa5a8d44c5aaf7631dbd3abe56
JoiLinCo <175733188+JoiLinCo@users.noreply.github.com>
2024-10-30 15:31:53 -0600

    Working game and guess count

--- script.js
+++ script.js
@@ -5,17 +5,26 @@ function submit() {
 
     let guessNumber = document.getElementById("guess").value;
     let guessFeedback;
+    let guessCount = guesses;
 
-    if (guessNumber < correctNumber) {
-        guessFeedback = "Too low. Try again. Guesses remaining: " + guesses;
-    } else if (guessNumber > correctNumber) {
-        guessFeedback = "Too high. Try again.";
+    if (guessCount === 0) {
+        guessFeedback = "You are out of guesses. Would you like to play again?";
+    } else if (guessNumber === "") {
+        guessFeedback = "Please enter an integer.";
     } else if (guessNumber == correctNumber) {
+        var element = document.getElementById("feedback");
+        element.classList.remove("feedback");
+        element.classList.add("correct");
         guessFeedback = "Congratulations!";
+    } else if (guessNumber < correctNumber) {
+        guessFeedback = "Too low. Try again. Guesses remaining: " + guesses;
+        guesses--;
+    } else if (guessNumber > correctNumber) {
+        guessFeedback = "Too high. Try again. Guesses remaining: " + guesses;
+        guesses--;
     } else {
         guessFeedback = "Try again.";
     }
-    
     
     document.getElementById("feedback").innerHTML = guessFeedback;
 
